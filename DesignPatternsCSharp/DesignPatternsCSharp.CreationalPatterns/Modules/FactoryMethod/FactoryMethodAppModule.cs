fd85f8f970aa544b24c714e780103d0b16b18d1a
Alexey Maslovsky <maslovskialexey@mail.ru>
2017-01-29 14:09:48 +0300

    added FactoryMethod

---
+++ DesignPatternsCSharp/DesignPatternsCSharp.CreationalPatterns/Modules/FactoryMethod/FactoryMethodAppModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DesignPatternsCSharp.BaseStructure.Interfaces;
+using DesignPatternsCSharp.CreationalPatterns.Modules.FactoryMethod.Body;
+
+namespace DesignPatternsCSharp.CreationalPatterns.Modules.FactoryMethod
+{
+    public class FactoryMethodAppModule : IAppModule
+    {
+        public void Start()
+        {
+            var family = new Family(3);
+
+            Console.WriteLine($"Family created. Member count = {family.CountMembersFamily}");
+
+            family.KillOneMember();
+
+            Console.WriteLine($"Minus 1. Now, member count = {family.CountMembersFamily}");
+
+            var child = family.CreateChild();
+
+            Console.WriteLine($"New chiled ! name = {child.Name}");
+            Console.WriteLine($"Plus 1. Now, member count = {family.CountMembersFamily}");
+        }
+    }
+}
