e6d7d39c6e21639490e92b3d374783c3879d80f2
Alexey Maslovsky <maslovskialexey@mail.ru>
2017-01-29 16:24:14 +0300

    added Prototype

---
+++ DesignPatternsCSharp/DesignPatternsCSharp.CreationalPatterns/Modules/Prototype/Body/Man.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DesignPatternsCSharp.CreationalPatterns.Modules.Prototype.Body
+{
+    public abstract class Man
+    {
+        public string Name { get; set; }
+
+        protected Man(string name)
+        {
+            Name = name;
+
+            Console.WriteLine($"Creating... Please wait");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");        
+            }
+
+            Console.WriteLine();
+        }
+
+        public abstract Man Clone();
+    }
+}
